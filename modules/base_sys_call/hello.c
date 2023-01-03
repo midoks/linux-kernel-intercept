cf7c4487d56e9a0e1019577996f4710522472c3c
midoks <midoks@163.com>
2023-01-03 12:11:38 +0800

    Update hello.c

--- modules/base_sys_call/hello.c
+++ modules/base_sys_call/hello.c
@@ -5,21 +5,63 @@
 
 #include <linux/kallsyms.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("midoks");
-MODULE_DESCRIPTION("hello");
-MODULE_VERSION("v1.0");
+
+
+static char readbuf[100];
+static char writebuf[100];
+
+static unsigned long * sys_call_table;
+
+asmlinkage int (*original_open) (const char*, int, int);
+asmlinkage int custom_open (const char* __user file_name, int flags, int mode);
 
 int __init hello_module_init(void)
 {
-    printk(KERN_INFO "Hello Kernel -- midoks .\n");
+    sys_call_table = NULL;
+    sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+    write_cr0 (read_cr0 () & (~ 0x10000));
+    original_open = (void *)sys_call_table[__NR_open];
+    sys_call_table[__NR_open] = custom_open;
+    write_cr0 (read_cr0 () | 0x10000);
+
+    printk(KERN_INFO "Hello Kernel loaded successfully -- midoks .\n");
     return 0;//A non 0 return means init_module failed; module can't be loaded.
 }
  
 void __exit hello_module_exit(void)
 {
+    write_cr0 (read_cr0 () & (~ 0x10000));
+    sys_call_table[__NR_open] = original_open;
+    write_cr0 (read_cr0 () | 0x10000);
+
     printk(KERN_INFO "Bye Kernel -- midoks .\n");
 }
  
+asmlinkage int custom_open (const char* __user file_name, int flags, int mode)
+{
+    copy_from_user(writebuf, file_name, 80);
+    printk ( KERN_INFO "hello %s\n", writebuf);
+    printk ( KERN_INFO "hello custom_open successfully %d- %d \n",flags, mode);
+    //获取当前进程的id
+    int pid = current->pid;
+    //获取当前进程的父进程id
+    int ppid = current->real_parent->real_parent->pid;
+    //获取当前进程的根目录
+    const char *ppwd = (current->fs->root).dentry->d_name.name;
+    struct path pwd;
+    //获取当前目录
+    get_fs_pwd(current->fs,&pwd);
+    printk(KERN_WARNING "helloa PID=%d,parent=%d attempts to open!\n",pid,ppid);
+    printk(KERN_WARNING "helloa ROOT:%s!\n",ppwd);
+    printk(KERN_WARNING "helloa PWD:%s!\n",pwd.dentry->d_name.name);
+    return original_open( file_name , flags, mode);
+}
+
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("midoks");
+MODULE_DESCRIPTION("hello");
+MODULE_VERSION("v1.0");
+
 module_init(hello_module_init);
 module_exit(hello_module_exit);
