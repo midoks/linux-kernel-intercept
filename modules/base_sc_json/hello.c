d1bae0830c83a2dd581c834836a3b03c6d29dc33
midoks <midoks@163.com>
2023-01-05 17:49:23 +0800

    Update hello.c

--- modules/base_sc_json/hello.c
+++ modules/base_sc_json/hello.c
@@ -67,7 +67,7 @@ asmlinkage int custom_mkdir(const char __user *pathname, int mode)
 {
     printk("mkdir pathname: %s\n", pathname);
     printk(KERN_ALERT "mkdir do nothing!\n");
-    return original_mkdir(file_name, mode);
+    return original_mkdir(pathname, mode);
     // return 0; /*everything is ok, but he new systemcall does nothing*/
 }
 
