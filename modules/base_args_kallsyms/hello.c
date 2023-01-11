23a1f1ad11adbb5019515635636ba2f22131be16
midoks <midoks@163.com>
2023-01-11 17:11:36 +0800

    up

---
+++ modules/base_args_kallsyms/hello.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>   /* Needed by all modules */
+#include <linux/kernel.h>   /* Needed for KERN_INFO */
+#include <linux/init.h>
+
+
+#include <linux/moduleparam.h>
+#include <linux/kallsyms.h>
+
+// MODULE_LICENSE("GPL");
+MODULE_AUTHOR("midoks");
+MODULE_DESCRIPTION("hello");
+MODULE_VERSION("v1.0");
+
+unsigned long (*n_kallsyms_lookup_name)(char *) = 0xffffffffb9b1ff50;
+// module_param(sys_call_table_address, charp, 0);
+// module_param(sys_call_table_address, charp, 0);
+
+int __init hello_module_init(void)
+{
+    printk(KERN_INFO "sys_call_table_address:%p\n", sys_call_table_address);
+    printk(KERN_INFO "Hello Kernel -- midoks .\n");
+    return 0;//A non 0 return means init_module failed; module can't be loaded.
+}
+ 
+void __exit hello_module_exit(void)
+{
+    printk(KERN_INFO "Bye Kernel -- midoks .\n");
+}
+ 
+module_init(hello_module_init);
+module_exit(hello_module_exit);
